9aa3063e83a6f8446cf13bd084907970b880599c
Vicly Xinyue Cao <viclycao1211@gmail.com>
2021-11-03 16:50:26 -0700

    Renamed images, some front end UI

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$("#card_1").attr("src","images/cards/C_1.png");
+$("#card_2").attr("src","images/cards/D_13.png");
+$("#host_1").attr("src","images/cards/back.png");
+$("#host_2").attr("src","images/cards/back.png");
+$("#host_3").attr("src","images/cards/back.png");
