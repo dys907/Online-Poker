dc0fcae1cc4e0753cb0dd4d70093a5818d0f3fc4
Dylan Sung <dys907@gmail.com>
2021-11-21 21:22:34 -0800

    testing swap merge

--- src/classes/player.js
+++ src/classes/player.js
@@ -1,7 +1,7 @@
 const Player = function (playerName, socket, debug) {
   this.username = playerName;
   this.cards = [];
-  this.powerUps = ['showCommunityCard', 'swapWithPlayer'];
+  this.powerUps = ['swapChipsWithPlayer', 'showCommunityCard'];
   this.socket = socket;
   this.currentCard = null;
   this.money = 100;
